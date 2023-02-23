0b637970c3d7a05451eba9de77d4e646de2e67f5
gajulaa <gajulaanirudha@gmail.com>
2023-02-23 22:15:35 +0530

    mod commit

--- Melbourne EDA.R
+++ Melbourne EDA.R
@@ -1,1 +1,6 @@
 #GENERAL INFORMATION ON DATASET######################################
+#
+#
+# This dataset is a snapshot of housing data from Domain. Domain is a website that
+# hosts publicly available real estate data from the Australian property market. 
+
